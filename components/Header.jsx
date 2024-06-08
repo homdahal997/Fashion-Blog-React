0cbd1f80ba4e704af1773d29b33b9f778cfafb35
Hom Dahal <homdahal997@gmail.com>
2024-06-08 16:55:52 -0400

    Header component created with navigation line and displayed on app.

---
+++ components/Header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <header>
+            <h1 className="topheadertext">Sartre's List</h1>
+            <h2>Better-Dressed People</h2>
+            <nav aria-label="Main Navigation" role="navigation">
+                <ul>
+                    <li><a href="">Women's</a></li>
+                    <li><a href="">Men's</a></li>
+                    <li><a href="">On the Street</a></li>
+                    <li><a href="">The Catwalk</a></li>
+                    <li><a href="">AdWatch</a></li>
+                    <li><a href="">About</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header;
