714e221acd9dba3b179a634c888bb762c85a4fe3
Hom Dahal <homdahal997@gmail.com>
2024-06-08 18:45:30 -0400

    Footer component added with some styling fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import BlogPost from '../components/BlogPost'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import './App.css'
 import image1 from './assets/blog-image-1.jpg'
@@ -14,6 +15,7 @@ function App() {
       <BlogPost date="11/11/20" title ="Vintage in Vogue" imgURL = {image2} content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod soluta nemo sunt, explicabo maxime facilis cum enim ab illum officiis. Recusandae vero excepturi dolores at unde, officia porro beatae iste eligendi nesciunt magni commodi dicta quibusdam enim tempora esse consequatur quasi assumenda!Repudiandae deserunt earum nemo numquam voluptates. Aliquam ullam saepe sequi, doloribus ipsam ad accusantium quia repellendus mollitia, dolore impedit harum quaerat asperiores incidunt totam exercitationem veniam! Porro exercitationem illo dicta quia. Omnis, vitae cumque optio unde consequatur, blanditiis corrupti eveniet a beatae voluptate sapiente placeat, hic dolor ratione sequi qui molestiae praesentium saepe inventore itaque necessitatibus ea. Obcaecati." postLinkURL = "#">
       </BlogPost>
     </main>
+    <Footer />
     </>
   )
 }
